3eb156bb053fa90d725fcdbff64a4d511a05d1f1
Reginaldo Junior <reginaldojr.dev@gmail.com>
2025-08-13 11:30:47 -0300

    Criação do esqueleto do projeto

---
+++ src/test/java/com/examplo/lab/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplo.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
